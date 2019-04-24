212fe832f95bce6d7a7e15970ef9c44ec5207828
Kaleb Batz <kbatz@vo-g2.21-school.ru>
2019-04-24 19:56:01 +0300

    almost norm

--- src/read.c
+++ src/read.c
@@ -6,7 +6,7 @@
 /*   By: kbatz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 17:21:08 by kbatz             #+#    #+#             */
-/*   Updated: 2019/04/24 18:48:31 by kbatz            ###   ########.fr       */
+/*   Updated: 2019/04/24 19:54:15 by kbatz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,97 +41,112 @@ void	fill_map_elem(int *elem, char **str)
 	}
 }
 
+int		ft_fill(t_params *prms, char *str, int *min, int *max)
+{
+	int		k;
+
+	prms->map = ft_realloc(prms->map, sizeof(*(prms->map)) * prms->y, 1);
+	k = 0;
+	while (*str)
+	{
+		while (ft_isspace(*str))
+			str++;
+		if (*str == '-' || *str == '+' || ft_isdigit(*str))
+		{
+			prms->map[prms->y] = ft_realloc(prms->map[prms->y], \
+					sizeof(**(prms->map)) * k, 1);
+			prms->map[prms->y][k] = ft_memalloc(sizeof(***(prms->map)) * 2);
+			fill_map_elem(prms->map[prms->y][k++], &str);
+			if (!prms->y && !(k - 1))
+			{
+				*min = prms->map[prms->y][k - 1][0];
+				*max = *min;
+			}
+			else if (prms->map[prms->y][k - 1][0] < *min)
+				*min = prms->map[prms->y][k - 1][0];
+			else if (prms->map[prms->y][k - 1][0] > *max)
+				*max = prms->map[prms->y][k - 1][0];
+		}
+	}
+	prms->y++;
+	return (k);
+}
+
+void	ft_fill_one(int *color, int min, int max)
+{
+	double	bk;
+
+	if (!*(color - 1))
+		*color = ZERO;
+	else if (*(color - 1) > 0)
+	{
+		bk = (double)*(color - 1) / (double)max;
+		*color = (int)round(((TOP >> 16) & 0xff) * bk + \
+				((ZERO >> 16) & 0xff) * (1 - bk)) << 16;
+		*color |= (int)round(((TOP >> 8) & 0xff) * bk + \
+				((ZERO >> 8) & 0xff) * (1 - bk)) << 8;
+		*color |= (int)round((TOP & 0xff) * bk + \
+				(ZERO & 0xff) * (1 - bk));
+	}
+	else if (*(color - 1) < 0)
+	{
+		bk = (double)*(color - 1) / (double)min;
+		*color = (int)round(((BOT >> 16) & 0xff) * bk + \
+				((ZERO >> 16) & 0xff) * (1 - bk)) << 16;
+		*color |= (int)round(((BOT >> 8) & 0xff) * bk + \
+				((ZERO >> 8) & 0xff) * (1 - bk)) << 8;
+		*color |= (int)round((BOT & 0xff) * bk + \
+				(ZERO & 0xff) * (1 - bk));
+	}
+}
+
+void	ft_fill_color(t_params *prms, int min, int max)
+{
+	int		i;
+	int		j;
+
+	j = prms->y;
+	while (j-- > 0)
+	{
+		i = prms->x;
+		while (i-- > 0)
+		{
+			if (!prms->map[j][i][1])
+				ft_fill_one(prms->map[j][i] + 1, min, max);
+			else
+				prms->map[j][i][1] &= 0x00ffffff;
+			prms->map[j][i][0] -= min;
+		}
+	}
+	prms->z = max - min;
+}
+
 void	ft_read(int fd, t_params *prms)
 {
 	char	*str;
-	char	*buf;
 	char	b;
-	int		***map;
-	int		len;
-	int		k;
 	int		min;
 	int		max;
-	int		i;
-	int		j;
-	double	bk;
+	int		k;
 
-	map = NULL;
-	len = 0;
-	while ((b = get_next_line(fd, &buf)) > 0)
+	while ((b = get_next_line(fd, &str)) > 0)
 	{
-		str = buf;
-		map = ft_realloc(map, sizeof(*map) * len, 1);
-		k = 0;
-		while (*str)
-		{
-			while (ft_isspace(*str))
-				str++;
-			if (*str == '-' || *str == '+' || ft_isdigit(*str))
-			{
-				map[len] = ft_realloc(map[len], sizeof(**map) * k, 1);
-				map[len][k] = ft_memalloc(sizeof(***map) * 2);
-				fill_map_elem(map[len][k++], &str);
-				if (!len && !(k - 1))
-				{
-					min = map[len][k - 1][0];
-					max = min;
-				}
-				else if (map[len][k - 1][0] < min)
-					min = map[len][k - 1][0];
-				else if (map[len][k - 1][0] > max)
-					max = map[len][k - 1][0];
-			}
-		}
-		len++;
-		free(buf);
+		k = ft_fill(prms, str, &min, &max);
+		free(str);
 		if (!prms->x)
 			prms->x = k;
 		else if (prms->x != k)
 		{
-			prms->map = map;
-			prms->y = len - 1;
+			--prms->y;
 			close(fd);
-			while (k-- > 0)
-				free(map[prms->y][k]);
-			free(map[prms->y]);
+			while (--k >= 0)
+				free(prms->map[prms->y][k]);
+			free(prms->map[prms->y]);
 			ft_exit(READ_ERROR, prms);
 		}
 	}
 	close(fd);
-	prms->map = map;
-	prms->y = len;
 	if (b == -1)
 		ft_exit(READ_ERROR, prms);
-	j = len;
-	while (j-- > 0)
-	{
-		i = k;
-		while (i-- > 0)
-		{
-			if (!map[j][i][1])
-			{
-				if (!map[j][i][0])
-					map[j][i][1] = ZERO;
-				else if (map[j][i][0] > 0)
-				{
-					bk = (double)map[j][i][0] / (double)max;
-					map[j][i][1] = (int)round(((TOP >> 16) & 0xff) * bk + ((ZERO >> 16) & 0xff) * (1 - bk)) << 16;
-					map[j][i][1] |= (int)round(((TOP >> 8) & 0xff) * bk + ((ZERO >> 8) & 0xff) * (1 - bk)) << 8;
-					map[j][i][1] |= (int)round((TOP & 0xff) * bk + (ZERO & 0xff) * (1 - bk));
-				}
-				else if (map[j][i][0] < 0)
-				{
-					bk = (double)map[j][i][0] / (double)min;
-					map[j][i][1] = (int)round(((BOT >> 16) & 0xff) * bk + ((ZERO >> 16) & 0xff) * (1 - bk)) << 16;
-					map[j][i][1] |= (int)round(((BOT >> 8) & 0xff) * bk + ((ZERO >> 8) & 0xff) * (1 - bk)) << 8;
-					map[j][i][1] |= (int)round((BOT & 0xff) * bk + (ZERO & 0xff) * (1 - bk));
-				}
-			}
-			else
-				map[j][i][1] &= 0x00ffffff;
-			map[j][i][0] -= min;
-		}
-	}
-	max -= min;
-	prms->z = max;
+	ft_fill_color(prms, min, max);
 }
